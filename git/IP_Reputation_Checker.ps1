bd5af871cef570411e5df0f09f90e4422f82240f
rahul shah <rahul.rbj3000@gmail.com>
2025-02-09 22:20:47 -0600

    concept

--- git/IP_Reputation_Checker.ps1
+++ git/IP_Reputation_Checker.ps1
@@ -1,1 +1,28 @@
-Write-Host "this is the beginning!"
+#$ip_add = Read-Host "Enter the IP addresses separated by commas : "
+$ip_add = @("78.109.200.147","59.173.135.46","98.51.132.203") 
+
+
+$VT_api = "" #insert your api here
+$VT_headers = @{"accept"="Application/JSON"; "x-apikey"="$VT_api"}
+
+
+
+foreach ($address in $ip_add) {
+
+    $VT_path = "https://www.virustotal.com/api/v3/ip_addresses/$address"
+    $VT_response = Invoke-WebRequest -Method Get -Uri $VT_path -Headers $VT_headers |ConvertFrom-Json
+    $AVs = @()
+    $total_flags = $VT_response.data.attributes.last_analysis_stats.malicious
+    if ($total_flags) {
+        
+        foreach ($property in $VT_response.data.attributes.last_analysis_results.PSobject.Properties) {
+            
+            if ($property.Value.category -eq "malicious") {
+                $AVs += $property.Name
+            }
+        }
+
+        Write-Host "$address is flagged Malicious by $total_flags scanners: $($AVs -join ", ")"
+
+    }
+}
